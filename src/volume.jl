50dd9f7a02fb9448af29910d8a6291e98f539487
sarahmschroeder <schroeder.sarahmaria@gmail.com>
2025-10-03 12:09:09 -0300

    Chama o LFrame

--- src/volume.jl
+++ src/volume.jl
@@ -40,7 +40,7 @@ function Derivada_volume(ne::Int64, dicionario_geometrias, dicionario_materiais,
     for e=1:ne
 
         # Dados que vamos precisar:
-        Ize, Iye, J0e, Ae, αe, Ee, Ge = Dados_fundamentais(e, dados_elementos, dicionario_materiais, dicionario_geometrias)
+        Ize, Iye, J0e, Ae, αe, Ee, Ge = LFrame.Dados_fundamentais(e, dados_elementos, dicionario_materiais, dicionario_geometrias)
 
         # dV fica:
         dv[e] = L[e]*Ae
